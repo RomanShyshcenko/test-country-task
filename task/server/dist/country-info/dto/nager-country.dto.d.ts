763f40134f94159073727211145582cd5f80a877
Roman <shyshchenkoroman@gmail.com>
2025-08-05 18:14:39 +0300

    delete dist

--- task/server/dist/country-info/dto/nager-country.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class NagerCountryDto {
-    countryCode: string;
-    name: string;
-}
--- task/server/dist/user/entities/user.entity.d.ts
+++
@@ -1,7 +0,0 @@
-import type { CalendarEvent } from '.';
-export declare class User {
-    id: number;
-    name: string;
-    email: string;
-    calendarEvents: CalendarEvent[];
-}
